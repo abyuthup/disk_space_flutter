c0aa3ec7db6a154b611688be5609ad10bb979c91
jithinpv <4jithinpv@gmail.com>
2024-08-30 11:18:36 +0530

    aby change

--- lib/disk_space.dart
+++ lib/disk_space.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 
 class DiskSpace {
   static const MethodChannel _channel = const MethodChannel('disk_space');
+  // aby
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
